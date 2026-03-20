20b35acac12c7f8a66d444b26315e80d2466f847
quentin.surdez <quentin.surdez@unil.ch>
2026-03-20 10:18:36 +0100

    feat: add strict concurrency flag to target

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftActorPOC",
+    platforms: [.macOS(.v10_15)],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "SwiftActorPOC"
+            name: "SwiftActorPOC",
+            swiftSettings: [
+                .unsafeFlags(["-strict-concurrency=complete"])
+            ]
         )
     ]
 )
